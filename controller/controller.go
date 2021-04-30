011379b36ec940eaf6c2eadbadb6a20d35021cc8
Enzo Grzeskiewicz <enzo.grzeskiewicz@ynov.com>
2021-04-30 09:51:13 +0200

    0.03

--- controller/controller.go
+++ controller/controller.go
@@ -22,34 +22,53 @@ func MakeRequest(url string) ([]byte, error) {
 	return []byte(body), err
 }
 
-func Init() {
-
-	start := time.Now()
-	fmt.Printf("\n")
+func artistsInit() {
 	artists, err := GetArtistsApi()
 	if err != nil {
 		fmt.Println(err)
 	}
+	fmt.Println("Artists done.")
 	fmt.Println((*artists)[0])
+}
 
+func datesInit() {
 	dates, err := GetConcertDates()
 	if err != nil {
 		fmt.Println(err)
 	}
+	fmt.Println("Dates done.")
 	fmt.Println(dates.Index[0].Dates)
-
+}
+func locationsInit() {
 	locations, err := GetLocations()
 	if err != nil {
 		fmt.Println(err)
 	}
+	fmt.Println("Locations done.")
 	fmt.Println(locations.Index[0].Locations)
-
+}
+func relationInit() {
 	relation, err := GetRelation()
 	if err != nil {
 		fmt.Println(err)
 	}
+	fmt.Println("Relation done.")
 	fmt.Println(relation.Index[0])
+}
 
+func Init() {
+
+	start := time.Now()
+	fmt.Printf("\n")
+
+	go artistsInit()
+	go datesInit()
+	go locationsInit()
+	go relationInit()
+
+	time.Sleep(600 * time.Millisecond)
+	// from 800ms to 600ms
 	elapsed := time.Since(start)
 	fmt.Printf("\ntook %s \n", elapsed)
+
 }
