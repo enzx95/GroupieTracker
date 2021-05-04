a96ccfbb050fea986f9e31f56d1c8b4ffdfa6cd8
Enzo Grzeskiewicz <enzo.grzeskiewicz@ynov.com>
2021-05-04 16:02:23 +0200

    Code Improvements

--- main.go
+++ main.go
@@ -3,36 +3,44 @@ package main
 import (
 	"GroupieTracker/controller"
 	"GroupieTracker/model"
+	"fmt"
 	"log"
 	"net/http"
 	"text/template"
+	"time"
 )
+
+var tableau [52]*model.Artist
+
+func makeTab() {
+	start := time.Now()
+	for i := 0; i < 52; i++ {
+		tableau[i] = controller.GetDataByID(i)
+	}
+	elapsed := time.Since(start)
+	fmt.Printf("\ntook %s \n", elapsed)
+
+}
 
 func mainPageHandler(w http.ResponseWriter, r *http.Request) {
 
-	tmpl, err := template.ParseFiles("index.html")
+	tmpl, err := template.ParseFiles("Artists.html")
 
 	if err != nil {
 		log.Fatal(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-
-	var tableau [3]*model.Artist
-	tableau[0] = controller.GetDataByID(0)
-	tableau[1] = controller.GetDataByID(1)
-	tableau[2] = controller.GetDataByID(2)
 
 	if err := tmpl.Execute(w, tableau); err != nil {
 		log.Fatal(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-
-	//tmpl.Execute(w, "Karim")
 
 }
 
 func main() {
 	controller.Init()
-	controller.GetDataByID(0)
-
+	makeTab()
 	http.HandleFunc("/", mainPageHandler)
 	http.ListenAndServe(":8080", nil)
 
