6026c97e23f91d5b434716c616a8a89df9baf0ca
Enzo Grzeskiewicz <enzo.grzeskiewicz@ynov.com>
2021-05-08 15:26:30 +0200

    Edited struct

--- model/artist.go
+++ model/artist.go
@@ -1,12 +1,13 @@
 package model
 
 type Artist struct {
-	Id               int64
-	Name             string
-	Image            string
-	Members          []string
-	CreationDate     uint16
-	ConcertLocations []string
-	ConcertDates     []string
-	Relation         map[string][]string
+	Id           int64
+	Name         string
+	Image        string
+	Members      []string
+	CreationDate uint16
+	ConcertDates []string
+	Relation     map[string][]string
+	Countries    []string
+	Cities       []string
 }
