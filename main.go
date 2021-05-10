8d8e2935fd7f104541d014dd0cdd9e70c959a33f
Enzo Grzeskiewicz <enzo.grzeskiewicz@ynov.com>
2021-05-10 18:02:30 +0200

    Added 404

--- main.go
+++ main.go
@@ -30,6 +30,11 @@ func join(s ...string) string {
 
 func mainPageHandler(w http.ResponseWriter, r *http.Request) {
 
+	if r.URL.Path != "/" || r.Method != "GET" {
+		errorHandler(w, r, http.StatusNotFound)
+		return
+	}
+
 	tmpl, err := template.New("Artists.html").Funcs(template.FuncMap{"join": join}).ParseFiles("./assets/pages/Artists.html")
 
 	if err != nil {
@@ -42,6 +47,21 @@ func mainPageHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
+}
+
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+	w.WriteHeader(status)
+	if status == http.StatusNotFound {
+		t, err := template.ParseFiles("./assets/pages/error.html")
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		err = t.Execute(w, "404 Not Found")
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	}
 }
 
 func main() {
